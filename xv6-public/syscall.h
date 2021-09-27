4232a46fca1054e0b13af918c8936bb08f068d7c
Maciel <maciel.salguero@mi.unc.edu.ar>
2021-09-27 17:03:41 -0300

    Agregadas syscalls a syscall.h y syscall.c

--- xv6-public/syscall.h
+++ xv6-public/syscall.h
@@ -20,6 +20,6 @@
 #define SYS_link          19
 #define SYS_mkdir         20
 #define SYS_close         21
-/* #define SYS_modeswitch    22
+#define SYS_modeswitch    22
 #define SYS_plotpixel     23
-#define SYS_plotrectangle 24 */
+#define SYS_plotrectangle 24
