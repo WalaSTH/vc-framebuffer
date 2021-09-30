1af52cc98ede2c26a8727648f5f32f256a6a87ab
Matías Javier Scantamburlo <matias.scantamburlo@mi.unc.edu.ar>
2021-09-30 11:45:11 -0300

    fe de errata

--- xv6-public/graphmode.c
+++ xv6-public/graphmode.c
@@ -3,15 +3,24 @@
 //#include "drawing.c"
 //#include "rock_tile.c"
 #include "redOctorok_tile.c"
-#include "link.c"
+#include "link_tile.c"
 
 int main(void){
-    modeswitch(1);
-    plotrectangle(0,0,320,200, 14);
+	modeswitch(1);
+	plotrectangle(0,0,320,200, 14);
     //print_rock_tile(0,0,2);
+    rock_tile_matrix(0,6,2,4,2);
+    rock_tile_matrix(14,6,2,4,2);
+    rock_tile_matrix(2,8,12,2,2);
     rock_tile_matrix(0,0,6,2,2);
+    rock_tile_matrix(0,2,2,2,2);
+    rock_tile_matrix(2,2,1,1,2);
     rock_tile_matrix(9,0,7,4,2);
-    drawRedO(100,100);
-    drawLink(120,100);
+    plotrectangle(60,20,80,40,0);
+    drawRedO1(80,100);
+    drawRedO2(100,100);
+    drawLink1(120,100);
+    drawLink2(140,100);
     exit();
 }
+
