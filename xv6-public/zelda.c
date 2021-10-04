7a300b66cabde8ca1e57dcc15e04d32daf059a73
Maciel <maciel.salguero@mi.unc.edu.ar>
2021-10-04 13:29:32 -0300

    Link no se choca de nuevo

--- xv6-public/zelda.c
+++ xv6-public/zelda.c
@@ -33,33 +33,44 @@ void draw_map0(int color){
     //Cave
     plotrectangle(80,20,100,40,0);
 }
-int good_move(int x, int y, int dir){
-    //dir cases:
-    //1 = up, 2 = right, 3 = left, 4 = right
+int good_move_up(int x, int y){
     char color;
     int ret = 1;
-    switch (dir)
-    {
-    case 1:
-        y = y - MO;
-        break;
-    case 2:
-        y = y + MO + HEIGHT;
-        break;
-    case 3:
-        x = x - MO;
-        break;
-    case 4:
-        x = x + MO + WIDTH;
-        break;
-    }
+    y = y - MO;
     for(int i = 0; i < WIDTH; ++i){
-        if(dir == 1 || 2){
-            color = (char)get_color(i+x,y);
-        }
-        else{
-            color = (char)get_color(x,i+y);
-        }
+        color = (char)get_color(i+x,y);
+        ret = ret && (color != 2 && color != 0);
+    }
+    return ret;
+}
+int good_move_down(int x, int y){
+    char color;
+    int ret = 1;
+    y = y + MO + HEIGHT;
+    for(int i = 0; i < WIDTH; ++i){
+        color = (char)get_color(i+x,y);
+        ret = ret && (color != 2 && color != 0);
+    }
+    return ret;
+}
+
+int good_move_left(int x, int y){
+    char color;
+    int ret = 1;
+    x = x - MO; 
+    for(int i = 0; i < WIDTH; ++i){
+        color = (char)get_color(x,i+y);
+        ret = ret && (color != 2 && color != 0);
+    }
+    return ret;
+}
+
+int good_move_right(int x, int y){
+    char color;
+    int ret = 1;
+    x = x + MO + WIDTH;
+    for(int i = 0; i < WIDTH; ++i){
+        color = (char)get_color(x,i+y);
         ret = ret && (color != 2 && color != 0);
     }
     return ret;
@@ -68,94 +79,89 @@ int good_move(int x, int y, int dir){
 int main(void){
     modeswitch(1);
     draw_map0(2);
-    int x, y, counter, play;
+    int x, y, counter;
     counter = 0;
     x = 120;
     y = 100;
-    play = 1;
     drawLinkD1(x,y);
-    while(play){
-        int key = getch();
-        switch (key)
-        {
-        case 'w':
-            /* move up */
-            //rectangle
-            plotrectangle(x,y, x+15, y+16, 14);
-
-            if(good_move(x,y,1)){
-                y = y - 2;
-                if(counter % 6 < 3){ 
-                    drawLinkU1(x,y);
-                    counter++; 
-                }else{
-                    drawLinkU2(x,y);
-                    counter++;
-                }
-            }
-            else{
-                drawLinkU2(x,y);  
-            }        
-            break;
-        case 'a':
-            plotrectangle(x,y, x+15, y+16, 14);
-            /* move left */                
-            if(good_move(x,y,3)){
-                x = x - 2;
-                if(counter % 6 < 3){ 
-                    drawLinkL1(x,y);
-                    counter++; 
-                }else{
-                    drawLinkL2(x,y);
-                    counter++;
-                }
-            }
-            else{
-                drawLinkL1(x,y);
-            } 
-            break;
-        case 's':
-            //rectangle        
-            plotrectangle(x,y, x+15, y+16, 14);        
-            // move dwn
-            if(good_move(x,y,2)){
-                y = y + 2;
-                if(counter % 6 < 3){ 
-                    drawLinkD1(x,y);
-                    counter++; 
-                }else{
-                    drawLinkD2(x,y);
-                    counter++;
-                }
-            }
-            else{
-                drawLinkD2(x,y);            
-            }         
-            break;
-        case 'd':
-            plotrectangle(x,y, x+15, y+16, 14);
-            /* move right */
-            if(good_move(x,y,4)){
-                x = x + 2;
-                if(counter % 6 < 3){ 
-                    drawLinkR1(x,y);
-                    counter++; 
-                }else{
-                    drawLinkR2(x,y);
-                    counter++;
-                }
-            }
-            else{
-                drawLinkR2(x,y);
-            }   
-            break;
-        case 'p':
-            play = 0;
-            break;
-        default:
-            break;
-        }
+    while(1){
+    int key = getch();
+    switch (key)
+    {
+    case 'w':
+        /* move up */
+        //rectangle
+        plotrectangle(x,y, x+15, y+16, 14);
         
+        if(good_move_up(x,y)){
+            y = y - 2;
+            if(counter % 6 < 3){ 
+                drawLinkU1(x,y);
+                counter++; 
+            }else{
+                drawLinkU2(x,y);
+                counter++;
+            }
+        }
+        else{
+            drawLinkU2(x,y);  
+        }        
+        break;
+    case 'a':
+        plotrectangle(x,y, x+15, y+16, 14);
+        /* move left */                
+        if(good_move_left(x,y)){
+            x = x - 2;
+            if(counter % 6 < 3){ 
+                drawLinkL1(x,y);
+                counter++; 
+            }else{
+                drawLinkL2(x,y);
+                counter++;
+            }
+        }
+        else{
+            drawLinkL1(x,y);
+        } 
+        break;
+    case 's':
+        //rectangle        
+        plotrectangle(x,y, x+15, y+16, 14);        
+        // move dwn
+        if(good_move_down(x,y)){
+            y = y + 2;
+            if(counter % 6 < 3){ 
+                drawLinkD1(x,y);
+                counter++; 
+            }else{
+                drawLinkD2(x,y);
+                counter++;
+            }
+        }
+        else{
+            drawLinkD2(x,y);            
+        }         
+        break;
+    case 'd':
+        plotrectangle(x,y, x+15, y+16, 14);
+        /* move right */
+        if(good_move_right(x,y)){
+            x = x + 2;
+            if(counter % 6 < 3){ 
+                drawLinkR1(x,y);
+                counter++; 
+            }else{
+                drawLinkR2(x,y);
+                counter++;
+            }
+        }
+        else{
+            drawLinkR2(x,y);
+        }   
+        break;
+    default:
+        break;
+    }
     //print_rock_tile(0,0,2);       
 /*     drawRedO1(80,100);
     drawRedO2(100,100);
@@ -168,7 +174,6 @@ int main(void){
     drawLinkL1(160,120);
     drawLinkL2(180,120);  */   
     }
-    modeswitch(0);
     exit();
 }
 
